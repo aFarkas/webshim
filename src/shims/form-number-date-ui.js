0c6995d87d8cc990ac5de7bb1c70c5fc42b24bfb
Philipp Paul <bobbor.pap@googlemail.com>
2012-04-20 13:01:18 +0000

    typo labelledby

--- src/shims/form-number-date-ui.js
+++ src/shims/form-number-date-ui.js
@@ -162,7 +162,7 @@ jQuery.webshims.register('form-number-date-ui', function($, webshims, window, do
 			});
 		}
 		if(shim.length == 1 && !$('*', shim)[0]){
-			shim.attr('aria-labeledby', curLabelID);
+			shim.attr('aria-labelledby', curLabelID);
 			attr.label.bind('click', function(){
 				shim.focus();
 				return false;
@@ -309,7 +309,7 @@ jQuery.webshims.register('form-number-date-ui', function($, webshims, window, do
 			
 			
 			
-			date.attr('aria-labeledby', attr.label.attr('id'));
+			date.attr('aria-labelledby', attr.label.attr('id'));
 			attr.label.bind('click', function(){
 				datePicker.focus();
 				return false;
@@ -487,7 +487,7 @@ jQuery.webshims.register('form-number-date-ui', function($, webshims, window, do
 			;
 			
 			$('span', range)
-				.attr('aria-labeledby', attr.label.attr('id'))
+				.attr('aria-labelledby', attr.label.attr('id'))
 			;
 			attr.label.bind('click', function(){
 				$('span', range).focus();
