c826d552548a551886d4030cdb05cb292d8d29f5
Alexander Farkas <info@corrupt-system.de>
2012-07-09 14:53:16 +0200

    fixes issue #134

--- src/shims/form-shim-extend.js
+++ src/shims/form-shim-extend.js
@@ -4,6 +4,7 @@ webshims.inputTypes = webshims.inputTypes || {};
 //some helper-functions
 var cfg = webshims.cfg.forms;
 var isSubmit;
+
 var isNumber = function(string){
 		return (typeof string == 'number' || (string && string == string * 1));
 	},
@@ -630,9 +631,9 @@ webshims.defineNodeNameProperty('form', 'noValidate', {
 	}
 });
 
-
 if($.browser.webkit && Modernizr.inputtypes.date){
 	(function(){
+		
 		var noInputTriggerEvts = {updateInput: 1, input: 1},
 			fixInputTypes = {
 				date: 1,
@@ -700,7 +701,83 @@ if($.browser.webkit && Modernizr.inputtypes.date){
 		;
 		if($.event.customEvent){
 			$.event.customEvent.updateInput = true;
-		} 
+		}
+		
+		(function(){
+			
+			var correctValue = function(elem){
+				var i = 1;
+				var len = 3;
+				var abort, val;
+				if(elem.type == 'date' && (isSubmit || !$(elem).is(':focus'))){
+					val = elem.value;
+					if(val && val.length < 10 && (val = val.split('-')) && val.length == len){
+						for(; i < len; i++){
+							if(val[i].length == 1){
+								val[i] = '0'+val[i];
+							} else if(val[i].length != 2){
+								abort = true;
+								break;
+							}
+						}
+						if(!abort){
+							val = val.join('-');
+							$.prop(elem, 'value', val);
+							return val;
+						}
+					}
+				}
+			};
+			var inputCheckValidityDesc, formCheckValidityDesc, inputValueDesc, inputValidityDesc;
+			
+			inputCheckValidityDesc = webshims.defineNodeNameProperty('input', 'checkValidity', {
+				prop: {
+					value: function(){
+						correctValue(this);
+						return inputCheckValidityDesc.prop._supvalue.apply(this, arguments);
+					}
+				}
+			});
+			
+			formCheckValidityDesc = webshims.defineNodeNameProperty('form', 'checkValidity', {
+				prop: {
+					value: function(){
+						$('input', this).each(function(){
+							correctValue(this);
+						});
+						return formCheckValidityDesc.prop._supvalue.apply(this, arguments);
+					}
+				}
+			});
+			
+			inputValueDesc = webshims.defineNodeNameProperty('input', 'value', {
+				prop: {
+					set: function(){
+						return inputValueDesc.prop._supset.apply(this, arguments);
+					},
+					get: function(){
+						return correctValue(this) || inputValueDesc.prop._supget.apply(this, arguments);
+					}
+				}
+			});
+			
+			inputValidityDesc = webshims.defineNodeNameProperty('input', 'validity', {
+				prop: {
+					writeable: false,
+					get: function(){
+						correctValue(this);
+						return inputValidityDesc.prop._supget.apply(this, arguments);
+					}
+				}
+			});
+			
+			$(document).bind('change', function(e){
+				isChangeSubmit = true;
+				correctValue(e.target);
+				isChangeSubmit = false;
+			});
+			
+		})();
 		
 		$(document)
 			.bind('focusin', function(e){
@@ -709,6 +786,8 @@ if($.browser.webkit && Modernizr.inputtypes.date){
 				}
 			})
 		;
+		
+		
 	})();
 }
 
