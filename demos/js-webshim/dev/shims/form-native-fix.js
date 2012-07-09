53a7e47437685968ec79303f45a828c262d6ea4a
aFarkas <info@corrupt-system.de>
2012-07-09 19:11:31 +0200

    compiled sources

--- demos/js-webshim/dev/shims/form-native-fix.js
+++ demos/js-webshim/dev/shims/form-native-fix.js
@@ -182,82 +182,80 @@ jQuery.webshims.register('form-native-fix', function($, webshims, window, doc, u
 		})();
 		
 		(function(){
-			var badVal = '2010-1-1';
-			if($('<input name="t" type="date" />').prop('value', badVal).prop('value') == badVal){
-				var correctValue = function(elem){
-					var i = 1;
-					var len = 3;
-					var abort, val;
-					if(elem.type == 'date' && (isChangeSubmit || !$(elem).is(':focus'))){
-						val = elem.value;
-						if(val && val.length < 10 && (val = val.split('-')) && val.length == len){
-							for(; i < len; i++){
-								if(val[i].length == 1){
-									val[i] = '0'+val[i];
-								} else if(val[i].length != 2){
-									abort = true;
-									break;
-								}
-							}
-							if(!abort){
-								val = val.join('-');
-								$.prop(elem, 'value', val);
-								return val;
+			
+			var correctValue = function(elem){
+				var i = 1;
+				var len = 3;
+				var abort, val;
+				if(elem.type == 'date' && (isChangeSubmit || !$(elem).is(':focus'))){
+					val = elem.value;
+					if(val && val.length < 10 && (val = val.split('-')) && val.length == len){
+						for(; i < len; i++){
+							if(val[i].length == 1){
+								val[i] = '0'+val[i];
+							} else if(val[i].length != 2){
+								abort = true;
+								break;
 							}
 						}
+						if(!abort){
+							val = val.join('-');
+							$.prop(elem, 'value', val);
+							return val;
+						}
 					}
-				};
-				var inputCheckValidityDesc, formCheckValidityDesc, inputValueDesc, inputValidityDesc;
-				
-				inputCheckValidityDesc = webshims.defineNodeNameProperty('input', 'checkValidity', {
-					prop: {
-						value: function(){
+				}
+			};
+			var inputCheckValidityDesc, formCheckValidityDesc, inputValueDesc, inputValidityDesc;
+			
+			inputCheckValidityDesc = webshims.defineNodeNameProperty('input', 'checkValidity', {
+				prop: {
+					value: function(){
+						correctValue(this);
+						return inputCheckValidityDesc.prop._supvalue.apply(this, arguments);
+					}
+				}
+			});
+			
+			formCheckValidityDesc = webshims.defineNodeNameProperty('form', 'checkValidity', {
+				prop: {
+					value: function(){
+						$('input', this).each(function(){
 							correctValue(this);
-							return inputCheckValidityDesc.prop._supvalue.apply(this, arguments);
-						}
+						});
+						return formCheckValidityDesc.prop._supvalue.apply(this, arguments);
 					}
-				});
-				
-				formCheckValidityDesc = webshims.defineNodeNameProperty('form', 'checkValidity', {
-					prop: {
-						value: function(){
-							$('input', this).each(function(){
-								correctValue(this);
-							});
-							return formCheckValidityDesc.prop._supvalue.apply(this, arguments);
-						}
+				}
+			});
+			
+			inputValueDesc = webshims.defineNodeNameProperty('input', 'value', {
+				prop: {
+					set: function(){
+						return inputValueDesc.prop._supset.apply(this, arguments);
+					},
+					get: function(){
+						return correctValue(this) || inputValueDesc.prop._supget.apply(this, arguments);
 					}
-				});
-				
-				inputValueDesc = webshims.defineNodeNameProperty('input', 'value', {
-					prop: {
-						set: function(){
-							return inputValueDesc.prop._supset.apply(this, arguments);
-						},
-						get: function(){
-							return correctValue(this) || inputValueDesc.prop._supget.apply(this, arguments);
-						}
+				}
+			});
+			
+			inputValidityDesc = webshims.defineNodeNameProperty('input', 'validity', {
+				prop: {
+					writeable: false,
+					get: function(){
+						correctValue(this);
+						return inputValidityDesc.prop._supget.apply(this, arguments);
 					}
-				});
-				
-				inputValidityDesc = webshims.defineNodeNameProperty('input', 'validity', {
-					prop: {
-						writeable: false,
-						get: function(){
-							correctValue(this);
-							return inputValidityDesc.prop._supget.apply(this, arguments);
-						}
-					}
-				});
-				
-				$(document).bind('change', function(e){
-					isChangeSubmit = true;
-					correctValue(e.target);
-					isChangeSubmit = false;
-				});
-			}
+				}
+			});
+			
+			$(document).bind('change', function(e){
+				isChangeSubmit = true;
+				correctValue(e.target);
+				isChangeSubmit = false;
+			});
+			
 		})();
 	}
-	
 	
 });
