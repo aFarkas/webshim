3aa387419d9b4b6fc5912731ea36045f8de7d87f
aFarkas <info@corrupt-system.de>
2013-11-06 18:21:34 +0100

    add experimentallyMimetypeCheck to mediaelement feature

--- src/shims/mediaelement-jaris.js
+++ src/shims/mediaelement-jaris.js
@@ -1099,53 +1099,60 @@ webshims.register('mediaelement-jaris', function($, webshims, window, document, 
 	} else if(!('media' in document.createElement('source'))){
 		webshims.reflectProperties('source', ['media']);
 	}
+	if(options.experimentallyMimetypeCheck){
+		(function(){
+			var ADDBACK = $.fn.addBack ? 'addBack' : 'andSelf';
+			var getMimeType = function(){
+				var done;
+				var unknown = 'media/unknown please provide mime type';
+				var media = $(this);
+				var xhrs = [];
+				media
+					.not('.ws-after-check')
+					.find('source')
+					[ADDBACK]()
+					.filter('[data-wsrecheckmimetype]:not([type])')
+					.each(function(){
+						var source = $(this).removeAttr('data-wsrecheckmimetype');
+						var error = function(){
+							source.attr('data-type', unknown);
+						};
+						try {
+							xhrs.push(
+								$.ajax({
+									type: 'head',
+									url: $.attr(this, 'src'),
+									success: function(content, status, xhr){
+										var mime = xhr.getResponseHeader('Content-Type');
+										if(mime){
+											done = true;
+										}
+										source.attr('data-type', mime || unknown);
+									},
+									error: error
+								})
+							)
+							;
+						} catch(er){
+							error();
+						}
+					})
+				;
+				if(xhrs.length){
+					media.addClass('ws-after-check');
+					$.when.apply($, xhrs).always(function(){
+						media.mediaLoad();
+						setTimeout(function(){
+							media.removeClass('ws-after-check');
+						}, 9);
+					});
+				}
+			};
+			$('audio.media-error, video.media-error').each(getMimeType);
+			$(document).on('mediaerror', function(e){
+				getMimeType.call(e.target);
+			});
+		})();
+	}
 	
-//	var ADDBACK = $.fn.addBack ? 'addBack' : 'andSelf';
-//	var getMimeType = function(){
-//		var done;
-//		var unknown = 'media/unknown please provide mime type';
-//		var media = $(this);
-//		var xhrs = [];
-//		media
-//			.not('.ws-after-check')
-//			.find('source')
-//			[ADDBACK]()
-//			.filter('[data-wsrecheckmimetype]')
-//			.each(function(){
-//				var source = $(this).removeAttr('data-wsrecheckmimetype');
-//				var error = function(){
-//					source.attr('type', unknown);
-//				};
-//				try {
-//					xhrs.push(
-//						$.ajax({
-//							type: 'head',
-//							url: $.attr(this, 'src'),
-//							success: function(content, status, xhr){
-//								done = true;
-//								source.attr('type', xhr.getResponseHeader('Content-Type') || unknown);
-//							},
-//							error: error
-//						})
-//					)
-//					;
-//				} catch(er){
-//					error();
-//				}
-//			})
-//		;
-//		if(xhrs.length){
-//			media.addClass('ws-after-check');
-//			$.when.apply($, xhrs).always(function(){
-//				media.mediaLoad();
-//				setTimeout(function(){
-//					media.removeClass('ws-after-check')
-//				}, 9);
-//			});
-//		}
-//	};
-//	$('audio.media-error, video.media-error').each(getMimeType);
-//	$(document).on('mediaerror', function(e){
-//		getMimeType.call(e.target);
-//	});
 });
