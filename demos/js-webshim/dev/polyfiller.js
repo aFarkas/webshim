eb8f60149f05cadc3673b1c432bd512db08b2828
Alexander.Frakas <info@corrupt-system.de>
2014-01-30 22:12:38 +0100

    1.12.2

--- demos/js-webshim/dev/polyfiller.js
+++ demos/js-webshim/dev/polyfiller.js
@@ -113,7 +113,7 @@
 	path = path.split('?')[0].slice(0, path.lastIndexOf("/") + 1) + 'shims/';
 
 	$.extend(webshims, {
-		version: '1.12.2-pre',
+		version: '1.12.2',
 		cfg: {
 			
 			//addCacheBuster: false,
