bd44e3214b2044b73cbc708f53732dea4f85a880
aFarkas <info@corrupt-system.de>
2014-08-12 11:22:11 +0200

    fix error in IE9-

--- src/shims/form-core.js
+++ src/shims/form-core.js
@@ -301,20 +301,6 @@ webshims.register('form-core', function($, webshims, window, document, undefined
 		}
 	});
 
-	$(function(){
-		var fileReaderReady = ('FileReader' in window && 'FormData' in window);
-		if(!fileReaderReady){
-			webshims.addReady(function(context){
-				if(!fileReaderReady && !modules.filereader.loaded && !modules.moxie.loaded){
-					if(context.querySelector('input.ws-filereader')){
-						webshims.reTest(['filereader', 'moxie']);
-						fileReaderReady = true;
-					}
-				}
-			});
-		}
-	});
-
 	if(options.addValidators && options.fastValidators){
 		webshims.reTest(['form-validators', 'form-validation']);
 	}
